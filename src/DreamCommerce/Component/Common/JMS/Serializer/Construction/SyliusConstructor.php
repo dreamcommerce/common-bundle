9e05bbc95045457f4630befab05a14e8d55b6dc9
Michał Korus <michal.korus@shoper.pl>
2017-05-05 16:55:39 +0200

    add sylius constructor for jms serializer

---
+++ src/DreamCommerce/Component/Common/JMS/Serializer/Construction/SyliusConstructor.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace DreamCommerce\Component\Common\JMS\Serializer\Construction;
+
+use Doctrine\Common\Persistence\ObjectManager;
+use JMS\Serializer\Metadata\ClassMetadata;
+use JMS\Serializer\Construction\ObjectConstructorInterface;
+use JMS\Serializer\DeserializationContext;
+use JMS\Serializer\VisitorInterface;
+use InvalidArgumentException;
+use Sylius\Component\Resource\Factory\FactoryInterface;
+use Sylius\Component\Resource\Metadata\RegistryInterface;
+use Sylius\Component\Resource\Repository\RepositoryInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class SyliusConstructor implements ObjectConstructorInterface
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    /**
+     * @var RegistryInterface
+     */
+    private $registry;
+
+    /**
+     * @var ObjectConstructorInterface
+     */
+    private $fallbackConstructor;
+
+    /**
+     * @param ContainerInterface $container
+     * @param RegistryInterface $registry
+     * @param ObjectConstructorInterface $fallbackConstructor
+     */
+    public function __construct(ContainerInterface $container, RegistryInterface $registry, ObjectConstructorInterface $fallbackConstructor)
+    {
+        $this->container = $container;
+        $this->registry = $registry;
+        $this->fallbackConstructor = $fallbackConstructor;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function construct(VisitorInterface $visitor, ClassMetadata $metadata, $data, array $type, DeserializationContext $context)
+    {
+        $syliusMetadata = null;
+        try {
+            $syliusMetadata = $this->registry->getByClass($metadata->name);
+        } catch(InvalidArgumentException $exception) {
+            // ignore
+        }
+
+        if($syliusMetadata !== null) {
+            $object = null;
+            if(isset($data['id'])) {
+                $repositoryId = $syliusMetadata->getServiceId('repository');
+                /** @var RepositoryInterface $repository */
+                $repository = $this->container->get($repositoryId);
+                $object = $repository->find($data['id']);
+
+                $managerId = $syliusMetadata->getServiceId('manager');
+                /** @var ObjectManager $manager */
+                $manager = $this->container->get($managerId);
+                $manager->initializeObject($object);
+            }
+
+            if($object === null) {
+                $factoryId = $syliusMetadata->getServiceId('factory');
+                /** @var FactoryInterface $factory */
+                $factory = $this->container->get($factoryId);
+                $object = $factory->createNew();
+            }
+
+            return $object;
+        }
+
+        return $this->fallbackConstructor->construct($visitor, $metadata, $data, $type, $context);
+    }
+}
