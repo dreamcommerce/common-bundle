49b9965305c2d205520ba451c12b6d9e4271ffb0
Michał Korus <michal.korus@shoper.pl>
2017-05-16 12:22:19 +0200

    add NotUniqueException

---
+++ src/DreamCommerce/Component/Common/Exception/NotUniqueException.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * (c) 2017 DreamCommerce
+ *
+ * @package DreamCommerce\Component\Common
+ * @author Michał Korus <michal.korus@dreamcommerce.com>
+ * @link https://www.dreamcommerce.com
+ */
+
+namespace DreamCommerce\Component\Common\Exception;
+
+use Exception;
+use Throwable;
+
+class NotUniqueException extends Exception implements ContextInterface
+{
+    use ContextTrait;
+
+    const CODE_PARAMETER_DEFINED = 10;
+
+    /**
+     * @var string
+     */
+    protected $parameterName;
+
+    /**
+     * @param string|null $parameterName
+     * @param Throwable $previousException
+     * @return NotUniqueException
+     */
+    public static function forParameter(string $parameterName = null, Throwable $previousException = null): NotUniqueException
+    {
+        $exception = new static('The parameter must be unique', static::CODE_PARAMETER_DEFINED, $previousException);
+        $exception->parameterName = $parameterName;
+
+        return $exception;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getParameterName()
+    {
+        return $this->parameterName;
+    }
+}
