0f8cdeddb324d56db787edfbb7f4e3c3ad83e6ea
Michał Korus <michal.korus@shoper.pl>
2017-05-31 19:06:15 +0200

    add extended xml element

---
+++ src/DreamCommerce/Component/Common/XML/ExtendedXMLElement.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace DreamCommerce\Component\Common\XML;
+
+use SimpleXMLElement;
+
+class ExtendedXMLElement extends SimpleXMLElement
+{
+    /**
+     * Add SimpleXMLElement code into a SimpleXMLElement
+     *
+     * @param SimpleXMLElement $append
+     */
+    public function appendXML(SimpleXMLElement $append)
+    {
+        if (strlen(trim((string)$append)) == 0) {
+            $xml = $this->addChild($append->getName());
+        } else {
+            $xml = $this->addChild($append->getName(), (string)$append);
+        }
+
+        foreach ($append->children() as $child) {
+            if($xml instanceof ExtendedXMLElement) {
+                $xml->appendXML($child);
+            }
+        }
+
+        foreach ($append->attributes() as $n => $v) {
+            $xml->addAttribute($n, $v);
+        }
+    }
+}
